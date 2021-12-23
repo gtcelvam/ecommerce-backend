066d60b985f5bd4e4b337492211bd54f0bf2a1b2
Gtcelvam <selvasizzler@gmail.com>
2021-12-23 13:45:24 +0530

    Changes on cart schema

--- models/Cart.js
+++ models/Cart.js
@@ -3,8 +3,15 @@ const mongoose = require("mongoose");
 const cartSchema = new mongoose.Schema({
     userId : {type:String,required:true},
     products : [{
-        productId : {type:String},
-        quantity : {type:Number,default:1}
+        title:{type:String,required:true},
+        desc:{type:String},
+        categories : {type:Array},
+        productId : {type:String,required:true},
+        size:{type:String,required:true},
+        color:{type:String,required:true},
+        quantity : {type:Number,default:1},
+        img:{type:String,required:true},
+        price:{type:Number,required:true}
     }]
 },
 {
